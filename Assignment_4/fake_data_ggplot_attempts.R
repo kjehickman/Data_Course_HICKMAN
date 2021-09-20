df80d6043222f470d2fc36ae9c02a1775c67dcd8
kjehickman <katelynjhickman@gmail.com>
2021-09-19 23:09:02 -0600

    This commit includes an updated (and slightly improved) version of assignment 4.

--- Assignment_4/fake_data_ggplot_attempts.R
+++ Assignment_4/fake_data_ggplot_attempts.R
@@ -1,5 +1,35 @@
 library(tidyverse)
 
+# essentially failed attempts
+
+# boxplot sucks with these... non-numerical
+ggplot(fakdat, aes(x=F_Class, y=B_Genus)) +
+  geom_boxplot() +
+  facet_wrap(~Group)
+
+ggplot(fakdat, aes(x=F_Class, y=B_Genus)) +
+  pairs(~F_Class + B_Genus + Plant_Part, col = factor(fakdat$F_Class, fakdat$B_Genus, fakdat$Plant_Part, fakdat$Group), data = fakdat) +
+  facet_wrap(~Group)
+
+fakdat %>% 
+  ggplot(mapping = aes(x=F_Class, y=Plant_Part)) +
+  geom_point(size = 3, alpha = 0.25) +
+  facet_wrap(~Group)  
+# how do you have two graphs with different variables next to each other? 
+# I obvs can't use 2 ggplot functions but I want different aes(). 
+# Can I do that by piping data to ggplot, then defining aes for each indv. gemo_?
+
+ggplot(fakdat, aes(x=F_Class, y=B_Genus, color=Plant_Part) + 
+         geom_jitter(width = 2, size = 2) +
+         labs(subtitle="Bacteria and Fungi by Plant Part",
+              x="Fungal Class",
+              y="Bacterial Genus",
+              title="Microbiome Correlation:"))
+
+# assignment 4 draft:
+
+library(tidyverse)
+
 # this set of functions creates the concatenations that will constitute my fake data set
 F_Class <- c("Agarico", "Entorrhizo", "Eurotio", "Eurotio", "Entorrhizo", "Sordario", "Agarico", "Sordario", "Eurotio", "Agarico", "Agarico", "Entorrhizo", "Entorrhizo", "Sordario", "Entorrhizo", "Eurotio")
 B_Genus <- c("Pleurocapsa", "Marinomonas", "Halomonas", "Tunicatimonas", "Halomonas", "Tunicatimonas", "Halomonas", "Pleurocapsa", "Halomonas", "Halomonas", "Halomonas", "Tunicatimonas", "Pleurocapsa", "Pleurocapsa")
@@ -10,10 +40,10 @@ Group <- c("Sonneratia", "Sonneratia", "Sonneratia", "Sonneratia", "Sonneratia",
 fakdat <- data.frame(cbind(F_Class, B_Genus, Plant_Part, Group))
 
 # having 3 categorical variables to correlate has been challenging to represent well... 
-# boxplots weren't helping, scatterplots didn't make much sense, bubble charts used 3+ if they were numerical, 
+# boxplots weren't helping, scatterplots didn't make much sense, bubble charts used 2 if they were numerical, 
 # pairwaise scatterplots and stacked bar plots (like in Lee et al.) used at least one numerical variable.
-# this ggplot utilizes geom_point() and separates into 4 graphs by plant part with intersections representing the presence of fungi & bacteria
-ggplot(fakdat, mapping = aes(x=B_Genus, y=F_Class, color = Group)) +
+# this ggplot utilizes geom_point() and separtes into 4 graphs by plant part with intersections representing the presence of fungi & bacteria
+ggplot(fakdat, mapping = aes(x=B_Genus, y=F_Class, color = Group)) +ggplot(fakdat, mapping = aes(x=B_Genus, y=F_Class, color = Group)) +
   geom_point(size = 3) +
   facet_wrap(~Plant_Part) +
   labs(subtitle="Bacteria and Fungi by Plant Part",
@@ -23,5 +53,6 @@ ggplot(fakdat, mapping = aes(x=B_Genus, y=F_Class, color = Group)) +
        title="Microbiome Correlation:") +
   theme_light()
 
-# this function exports the fake data to a csv file. I removed the full file path, so it should work on any computer.
-write.csv(fakdat, "fake_data.csv", row.names = FALSE)
+# this function exports the fake data to a csv file
+write.csv(fakdat, "/Users/mac/Desktop/Data_Course_HICKMAN/Assignment_4/fake_data.csv", row.names = FALSE)
+
