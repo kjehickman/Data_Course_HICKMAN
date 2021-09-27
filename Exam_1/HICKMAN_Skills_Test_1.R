0098fd4c15282ad7cb6d954b7f2f484bb6127c5d
kjehickman <katelynjhickman@gmail.com>
2021-09-26 23:43:03 -0600

    This includes a better version of the Bonus question.

--- Exam_1/HICKMAN_Skills_Test_1.R
+++ Exam_1/HICKMAN_Skills_Test_1.R
@@ -55,33 +55,30 @@ state_max_fatality_ratio2 %>%
   ggplot(mapping = aes(x=Province_State, y=Maximum_Fatality_Ratio)) +
   geom_bar(mapping = aes_(x=state_max_fatality_ratio2$Province_State, 
                           y=state_max_fatality_ratio2$Maximum_Fatality_Ratio), 
-           stat = "identity", fill="skyblue", alpha=0.75) +
+           stat = "identity", fill="black", alpha=0.9) +
   labs(x="Region", 
        y="Maximum Fatality Ratios", 
        title = "Maximum Fatality Ratios by State",
        caption = "Max fatality rates vary greatly by region, \ndefined as states and the District of Columbia.") +
   theme(plot.caption = element_text(size = 10), 
                plot.title = element_text(hjust = 0, colour = "blue", face = "bold"),
-        axis.text.x = element_text(angle = 90))
+        axis.text.x = element_text(angle = 90), panel.background = element_rect(fill = "green"))
 
 library(dplyr)
-# Step VI. BONUS
+# Step VI. BONUS; Fix ticks
 deathsplot <- df %>% 
   group_by(Last_Update, Deaths) %>% 
   summarize()
 
 
   ggplot(deathsplot, aes(x=Last_Update, y=Deaths)) +
-  geom_point() +
-  geom_smooth(se=FALSE) +
+    geom_bar(mapping = aes_(x=deathsplot$Last_Update, 
+                            y=deathsplot$Deaths), 
+             stat = "identity", fill="turquoise", alpha=0.75) +
     labs(x="Date", 
          y="Deaths", 
          title = "COVID-19 Deaths Over Time",
-         caption = "Deaths steadily increased over time throughout the US.")
-  
-  
-    theme(axis.text.x = element_text(angle = 90)) +
-    scale_x_continuous("Deaths", breaks = deathsplot$Last_Update) 
-    
+         caption = "Deaths steadily increased over time throughout the US.") +
+    theme(axis.text.x = element_text(angle = 90))
 
 
